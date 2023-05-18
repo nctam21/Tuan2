3163ce932d77e3a48b9cd1a26d80ffa05c3eedf6
quangneyyy <quang.phanvinh@meowhouse.vn>
2023-05-18 14:53:42 +0700

    continue-project

--- src/main/java/com/example/Tuan2/Controller/productsController.java
+++ src/main/java/com/example/Tuan2/Controller/productsController.java
@@ -1,40 +1,55 @@
 package com.example.Tuan2.Controller;
 
-import com.example.Tuan2.entity.Book;
+//import com.example.Tuan2.entity.Book;
 import com.example.Tuan2.entity.Product;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Iterator;
 import java.util.List;
 
+@Controller
+@RequestMapping("/products")
 public class productsController {
     @Autowired
     private List<Product> products;
     @GetMapping("")
-    public String listProduct( Model model){
+    public String listProducts( Model model){
         model.addAttribute("products", products);
         model.addAttribute("title", "Product List");
         return "product/list";
     }@GetMapping("/add")
-    public String addBookForm(Model model){
+    public String addProductForm(Model model){
         model.addAttribute("product",new Product());
         return "product/add";
     }
 
+//    @PostMapping("/add")
+//    public String addProduct(@ModelAttribute("product")Product product, @Valid Product newProduct, BindingResult result){
+//        if (result.hasErrors()) {
+//            return "products/add";
+//        }
+//        products.add(product);
+//        return "redirect:/products";
+//    }
+
     @PostMapping("/add")
-    public String addProduct(@ModelAttribute("product")Product product){
-        products.add(product);
+    public String addProduct(@Valid Product newProduct, BindingResult result, Model model){
+        if (result.hasErrors()) {
+            model.addAttribute("product", newProduct);
+            return "products/add";
+        }
+        products.add(newProduct);
         return "redirect:/products";
     }
 
 
     @GetMapping("/edit/{id}")
-    public String editBookForm(@PathVariable("id")Long id, Model model){
+    public String editProductForm(@PathVariable("id")Long id, Model model){
         Product editProduct = null;
         for (Product product : products){
 //            if (product.getId().equals(id)){
@@ -50,7 +65,7 @@ public class productsController {
         }
     }
     @PostMapping("/edit")
-    public String editBook(@ModelAttribute("product") Product updatedProduct){
+    public String editProduct(@ModelAttribute("product") Product updatedProduct){
         for (int i=0; i < products.size(); i++){
             Product product = products.get(i);
             if (product.getId() == updatedProduct.getId()){
--- src/main/java/com/example/Tuan2/entity/Product.java
+++ src/main/java/com/example/Tuan2/entity/Product.java
@@ -1,11 +1,24 @@
 package com.example.Tuan2.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class Product {
     private int id;
+
+    @NotBlank(message = "Tên sản phẩm không được để trống")
     private String name;
+
+    @Length(min = 0, max = 50, message = "tên hình ảnh không quá 50 ký tự")
     private String image;
+
+    @NotNull(message = "Giá sản phẩm không được để trống")
+    @Min(value = 1, message = "Giá sản phẩm không được nhỏ hơn 1")
+    @Max(value=99999999, message = "Giá sản phẩm không được lớn hơn 99999999")
     private long price;
 }
